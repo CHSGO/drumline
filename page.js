38dfccd09ad9541f88c747fc92ee197952e13597
pie902 <pie902@wwsd.ca>
2022-04-15 10:38:26 -0600

    FoOtEr IsSUeS

--- page.js
+++ page.js
@@ -1,5 +1,19 @@
 let header = '<div id="Head"><p id="Title">Cymbals</p></div><div id="Head2"></div>';
 document.getElementById("header").innerHTML = header;
 
-let footer = '<footer class="Foot"><p id="FooterText">The Ultimate Cymbal Guidebook for High School Drumline<br><br>High Five<br>Glitch</p></footer>';
+let footer = '<footer id="Foot" class="Foot"> <p id="FooterText">The Ultimate Cymbal Guidebook for High School Drumline<br><br>High Five<br>Glitch</p> </footer>';
 document.getElementById("footer").innerHTML = footer;
+
+//function fill()
+//{
+//    var Bodyh = document.getElementById("Body").style.height;
+//    var Menuh = document.getElementById("Menu").style.height;
+//    var Footh = document.getElementById("Foot").style.height;
+//    var Fillh = document.getElementById("Fill").style.height;
+//    
+//    Fillh = "30px";
+//    //Fillh = Bodyh - Menuh - Footh;
+//    console.log("Yes");
+//}
+
+//setInterval(fill, 100)
