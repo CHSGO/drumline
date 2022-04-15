9c7a14def47dd786b8d2de730838e9abd541e85e
pie902 <pie902@wwsd.ca>
2022-04-14 20:52:00 -0600

    update

--- page.js
+++ page.js
@@ -1,5 +1,5 @@
 let header = '<div id="Head"><p id="Title">Cymbals</p></div><div id="Head2"></div>';
 document.getElementById("header").innerHTML = header;
 
-let footer = '<div class="break"></div><footer class="Foot"><p id="FooterText">The Ultimate Cymbal Guidebook for High School Drumline<br><br>High Five<br>Glitch</p></footer>';
+let footer = '<footer class="Foot"><p id="FooterText">The Ultimate Cymbal Guidebook for High School Drumline<br><br>High Five<br>Glitch</p></footer>';
 document.getElementById("footer").innerHTML = footer;
